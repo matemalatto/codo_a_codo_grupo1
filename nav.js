228a66f6afcb80435893fea7195267660feda1c4
aldanas2l <ldncastano@gmail.com>
2024-05-16 22:55:30 -0300

    p

--- nav.js
+++ nav.js
@@ -1,22 +1,26 @@
 // Verificar el ancho de la pantalla
 function checkScreenWidth() {
     if (window.matchMedia("(max-width: 850px)").matches) {
-     
+   
         var enlaces = document.querySelectorAll('.ancla');
         enlaces.forEach(function(enlace) {
-            enlace.addEventListener('click', function() {
+            enlace.addEventListener('click',() => {
                 var navbar = document.querySelector('ul');
                 navbar.style.display = 'none';
-
+              
+             });
+              })
+         
                 var check = document.getElementById('check');
-                check.addEventListener('click', function() {
+                check.addEventListener('click',()=> {
                     var navbar = document.querySelector('ul');
-                    navbar.style.display = 'block';
-                });
-            });
-        });
+                    navbar.style.display = 'block'; });
+
+            }   
+           
+     
     }
-}
+
 
 
 window.onload = checkScreenWidth;
